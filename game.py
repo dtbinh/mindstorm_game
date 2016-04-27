a170d12c4acec3d713f3b19e65f6397c4cf07194
Hoogerino <basisten123@gmail.com>
2016-04-27 14:56:30 +0200

    Commented code for troubleshooting

--- game.py
+++ game.py
@@ -31,15 +31,15 @@ def colorCheck(sensor):
 def motor(direction):
     if direction == 'right':
         motorA.run_timed(time_sp=2000, duty_cycle_sp=50)
-        colorCheck(colorSensor.color())
+        # colorCheck(colorSensor.color())
     elif direction == 'left':
         motorB.run_timed(time_sp=2000, duty_cycle_sp=50)
-        colorCheck(colorSensor.color())
+        # colorCheck(colorSensor.color())
     elif direction == 'forward':
         motorA.run_timed(time_sp=2000, duty_cycle_sp=50)
         motorB.run_timed(time_sp=2000, duty_cycle_sp=50)
-        colorCheck(colorSensor.color())
+        # colorCheck(colorSensor.color())
     elif direction == 'backward':
         motorA.run_timed(time_sp=2000, duty_cycle_sp=-50) #check negative if this doesn't work
         motorB.run_timed(time_sp=2000, duty_cycle_sp=-50)
-        colorCheck(colorSensor.color())
+        # colorCheck(colorSensor.color())
