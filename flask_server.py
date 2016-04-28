d92fec9090f5072cc651302fd03422e4db519d23
Lukas B <slashsweden@gmail.com>
2016-04-28 15:01:25 +0200

    Changed name of game.py to controller.py

--- flask_server.py
+++ flask_server.py
@@ -18,11 +18,29 @@ Team set up
 Maybe some kind of controll gui like qt or tkinter for host?
 """
 from flask import Flask
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, make_response
 from random import randint
 # This is a local file (game.py) and will probely change name in the near
 # future
-import game
+import controller
+#import sqlite3
+
+#db_file = sqlite3.connect('database.db')
+#db = db_file.cursor()
+
+#db.execute('DROP TABLE players')
+#db.execute('CREATE TABLE players (id int, ip text, team text)')
+
+#def new_player(ip, team):
+#    count = 1
+#    db.execute("INSERT INTO players VALUES (?, ?, ?)", count, str(ip), team)
+#    db_file.commit()
+#    count += 1
+
+#def get_ip():
+#  response = make_response(request.remote_addr)
+#  response.mimetype = 'text/plain'
+#  return response
 
 def team():
     # Replace this function with something that is more permenant
@@ -33,22 +51,27 @@ def team():
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    team_var = team()
+    #print(get_ip())
+    #new_player(None, team_var)
+    return render_template('index.html', team=team_var)
+
 @app.route('/game', methods=['GET', 'POST'])
 def game():
+    #team = db.execute("SELECT team FROM players WHERE ip=?", get_ip())
     if request.method == 'POST':
         # Add functions before returning to run commands
         if request.form['submit'] == 'forward':
-            game.motor('forward')
+            controller.motor('forward')
             return render_template('game.html',team=team(), direction='forward')
         elif request.form['submit'] == 'left':
-            game.motor('left')
+            controller.motor('left')
             return render_template('game.html',team=team(), direction='left')
         elif request.form['submit'] == 'right':
-            game.motor('right')
+            controller.motor('right')
             return render_template('game.html',team=team(), direction='right')
         elif request.form['submit'] == 'backward':
-            game.motor('backward')
+            controller.motor('backward')
             return render_template('game.html',team=team(), direction='backward')
         else:
             return render_template('game.html',team=team(), direction='none')
@@ -56,3 +79,5 @@ def game():
         return render_template('game.html',team=team(), direction='none')
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+db_file.close()
