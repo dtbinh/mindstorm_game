3cc166117c73d1e567cc8761ac795ebb5cf1415b
zeroxfourc <ludvighg1999@gmail.com>
2016-05-03 13:52:38 +0200

    wruhgrwiouh

--- controller.sh
+++ controller.sh
@@ -2,9 +2,12 @@
 
 MotorA="/sys/class/tacho-motor/motor1"
 MotorB="/sys/class/tacho-motor/motor2"
-echo reset > $MotorA/command > $MotorB/command
-echo coast > $MotorA/stop_command > $MotorB/stop_command
-echo 2000 > $MotorA/time_sp > $MotorB/time_sp
+echo reset > $MotorA/command
+echo reset > $MotorB/command
+echo coast > $MotorA/stop_command
+echo coast > $MotorB/stop_command
+echo 2000 > $MotorA/time_sp
+echo 2000 > $MotorB/time_sp
 
 echo "PID: $$"
 
@@ -13,18 +16,24 @@ trap "read" SIGINT
 while true ; do
   read -n 1 command
   if [[ "$command" == "f" ]] ; then
-    echo 50 > $MotorA/duty_cycle_sp > $MotorB/duty_cycle_sp
-    echo run-timed > $MotorA/command > $MotorB/command
+    echo 50 > $MotorA/duty_cycle_sp
+    echo 50 > $MotorB/duty_cycle_sp
+    echo run-timed > $MotorA/command
+    echo run-timed > $MotorB/command
   elif [[ "$command" == "b" ]] ; then
-    echo -50 > $MotorA/duty_cycle_sp > $MotorB/duty_cycle_sp
-    echo run-timed > $MotorA/command > $MotorB/command
+    echo -50 > $MotorA/duty_cycle_sp
+    echo -50 > $MotorB/duty_cycle_sp
+    echo run-timed > $MotorA/command
+    echo run-timed > $MotorB/command
   elif [[ "$command" == "l" ]] ; then
     echo 50 > $MotorA/duty_cycle_sp
     echo -50 > $MotorB/duty_cycle_sp
-    echo run-timed > $MotorA/command > $MotorB/command
+    echo run-timed > $MotorA/command
+    echo run-timed > $MotorB/command
   elif [[ "$command" == "r" ]] ; then
     echo -50 > $MotorA/duty_cycle_sp
     echo 50 > $MotorB/duty_cycle_sp
-    echo run-timed > $MotorA/command > $MotorB/command
+    echo run-timed > $MotorA/command
+    echo run-timed > $MotorB/command
   fi
 done
