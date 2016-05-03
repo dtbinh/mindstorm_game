0777b5eaeafa7611dc8f4a604642939764134cae
zeroxfourc <ludvighg1999@gmail.com>
2016-05-03 15:12:17 +0200

    jfiwejfoiwjfuwjhefiuwhefiuhwguheiufhweuifhweughewfhweiufhweuifhweuifhewuifhweiufhweuifhweuifhwuifhwuifhweiufhewiufhweuifhweiufhiuewfh

--- color.sh
+++ color.sh
@@ -6,7 +6,7 @@ echo COL-COLOR > $Sensor1/mode
 while true ; do
   color="$( cat $Sensor1/value0 )"
   for value in 2 3 4 5 ; do
-    if [[ "$color" = "$value" ]] ; then
+    if [ "$color" = "$value" ] ; then
       beep -f 261.63 -l 100 -n -f 392 -l 400
       echo "team $color won!"
       read -s
