2a8ac636a8d6580a82307aac8f31a2f566569f06
zeroxfourc <ludvighg1999@gmail.com>
2016-05-04 09:54:32 +0200

    export

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-MotorA="/sys/class/tacho-motor/motor1"
-MotorB="/sys/class/tacho-motor/motor2"
+export MotorA="/sys/class/tacho-motor/motor1"
+export MotorB="/sys/class/tacho-motor/motor2"
 echo reset > $MotorA/command
 echo reset > $MotorB/command
 echo coast > $MotorA/stop_command
