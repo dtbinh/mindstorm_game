cd0a74ed705ad0c30cef4625a8d835b9c7507f29
zeroxfourc <ludvighg1999@gmail.com>
2016-05-04 10:06:58 +0200

    efj

--- controller.sh
+++ controller.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+MotorA="/sys/class/tacho-motor/motor0"
+MotorB="/sys/class/tacho-motor/motor1"
+echo reset > $MotorA/command
+echo reset > $MotorB/command
+echo coast > $MotorA/stop_command
+echo coast > $MotorB/stop_command
+echo 1000 > $MotorA/time_sp
+echo 1000 > $MotorB/time_sp
+
 command="$1"
 
 if [[ "$command" == "forward" ]] ; then
