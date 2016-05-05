1945a091f795e5bbb540df89e9ba145d320bc53a
zeroxfourc <ludvighg1999@gmail.com>
2016-05-05 14:33:01 +0200

    AND HIS NAME IS

--- color.sh
+++ color.sh
@@ -7,10 +7,8 @@ while true ; do
   color="$( cat $Sensor1/value0 )"
   for value in 2 3 4 5 ; do
     if [ "$color" = "$value" ] ; then
-      beep -f 261.63 -l 100 -n -f 392 -l 400
-      echo "team $color won!"
-      read
+      beep -f 415.3 -l 300 -n -f 466.16 -l 50 -n -f 369.99 -l 100 -n -f 415.3 -l 300 -n -f 493.88 -l 300 -n -f 466.16 -l 50 -n -f 369.99 -l 100 -n -f 415.3 -l 300 
     fi
   done
-  sleep .5
+  #sleep .5
 done
