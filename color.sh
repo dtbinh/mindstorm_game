0b6d70ff2eed908fe8f69a13df9968d58c69b09b
zeroxfourc <ludvighg1999@gmail.com>
2016-05-05 14:43:24 +0200

    JOHN

--- color.sh
+++ color.sh
@@ -7,7 +7,7 @@ while true ; do
   color="$( cat $Sensor1/value0 )"
   for value in 2 3 4 5 ; do
     if [ "$color" = "$value" ] ; then
-      beep -f 415.3 -l 300 -n -f 466.16 -l 50 -n -f 369.99 -l 100 -n -f 415.3 -l 300 -n -f 493.88 -l 300 -n -f 466.16 -l 50 -n -f 369.99 -l 100 -n -f 415.3 -l 300 
+      beep -f 415.3 -l 300 -n -f 466.16 -l 200 -n -f 369.99 -l 300 -n -f 415.3 -l 400 -n -f 493.88 -l 300 -n -f 466.16 -l 200 -n -f 369.99 -l 300 -n -f 415.3 -l 400 
     fi
   done
   #sleep .5
